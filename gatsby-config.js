db58a8443887dc05a32a9873c10587811ebb5798
Berta Matu <info.berta@gmail.com>
2020-05-22 21:56:30 +0300

    bertamatu v1.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,9 +54,6 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
       },
     },
     {
