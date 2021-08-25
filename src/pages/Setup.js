2fc912d1de4a1e13c365b37c4a5e1e4e27e8d4ab
Berta Matuliauskiene <berta.matuliauskiene@satalia.com>
2021-08-25 12:22:35 +0300

    setup page edit

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -7,8 +7,7 @@ const Setup = () => {
             <div
                 style={{
                     width: '100vw',
-                    // height: '100vh',
-                    fontSize: '14px',
+                    fontSize: '24px',
                     position: 'absolute',
                     top: '20vh',
                     color: 'black',
@@ -18,18 +17,47 @@ const Setup = () => {
                     flexDirection: 'column',
                     fontWeight: '900',
                     paddingTop: '140px',
+                    fontFamily: 'Roboto',
                 }}
             >
-                <p style={{ margin: '0 auto' }}>... UNDER CONSTRUCTION ...</p>
-                <img
+                <p
                     style={{
-                        height: '250px',
-                        width: '250px',
                         margin: '0 auto',
+                        color: 'salmon',
+                        textAlign: 'center',
                     }}
-                    src="https://media.giphy.com/media/DS89v1NqpzCqA/giphy.gif"
-                    alt="soon"
+                >
+                    Sorry for the dust!
+                    <br />
+                    The setup section is COMMING SOON!
+                    <h6>
+                        ... in the meantime, you could share your favorite music
+                        with me by adding songs to this playlist :)
+                    </h6>
+                </p>
+                <br />
+
+                <iframe
+                    src="https://open.spotify.com/embed/playlist/7pDnkIkcMzSU6r3Op51qeH"
+                    width="300"
+                    height="380"
+                    frameborder="0"
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                    title="bertacodes"
+                    style={{ filter: 'grayscale(100%)', margin: '0 auto' }}
                 />
+
+                {/* <iframe
+                    src="https://open.spotify.com/embed/playlist/2ZiQSPtyue1RmH6mjXLnWj"
+                    width="300"
+                    height="380"
+                    frameborder="0"
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                    title="bertacodes"
+                    style={{ filter: 'grayscale(100%)', margin: '0 auto' }}
+                ></iframe> */}
             </div>
         </Layout>
     );
