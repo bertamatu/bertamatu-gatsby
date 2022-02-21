38ddeec467c3e4c860f66b0e759f13955a1fae7d
Berta <info.berta@gmail.com>
2022-02-22 01:23:08 +0200

    chore: disable gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import Posts from 'react-ig-feed';
-import 'react-ig-feed/dist/index.css';
-import styled from 'styled-components';
+// import React from 'react';
+// import Layout from '../components/Layout';
+// import Posts from 'react-ig-feed';
+// import 'react-ig-feed/dist/index.css';
+// import styled from 'styled-components';
 
-const MainContent = styled.main`
-    width: 100vw;
-    position: absolute;
-    top: 35%;
-    z-index: -1;
-`;
+// const MainContent = styled.main`
+//     width: 100vw;
+//     position: absolute;
+//     top: 35%;
+//     z-index: -1;
+// `;
 
-const GalleryPage = () => {
-    return (
-        <Layout>
-            <MainContent>
-                <Posts token={process.env.GATSBY_POSTS} counter="12" />
-            </MainContent>
-        </Layout>
-    );
-};
+// const GalleryPage = () => {
+//     return (
+//         <Layout>
+//             <MainContent>
+//                 <Posts token={process.env.GATSBY_POSTS} counter="12" />
+//             </MainContent>
+//         </Layout>
+//     );
+// };
 
-export default GalleryPage;
+// export default GalleryPage;
