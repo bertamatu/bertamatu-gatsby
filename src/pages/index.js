d89624c4c07ca24830120619696459c761fb7d63
Berta <info.berta@gmail.com>
2022-03-11 11:24:21 +0200

    feat: change home page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,40 +6,48 @@ import SocialIcons from '../components/buttons/SocialIcons';
 import HomePageLogo from '../components/common/HomePageLogo';
 import TechIcons from '../components/common/TechIcons';
 import ConnectButton from '../components/buttons/Connect';
+import Flowers from '../data/images/flowersBckg.png'
 
 const IndexPage = () => (
     <HomePage>
         <Layout>
             <Seo title="Home" />
             <HomePageLogo />
-            <Image
-                src="https://media.giphy.com/media/5UutpfMIjPbZGI4van/source.gif"
-                alt="bertacodes"
-            />
             <TechIcons />
             <ConnectButton />
             <SocialIcons />
             <AdditionalText>z e r o t o m a s t e r y</AdditionalText>
         </Layout>
+        <Image
+          src={Flowers}
+          alt="bertacodes"
+        />
     </HomePage>
 );
 
 const HomePage = styled.section`
     overflow-y: hidden;
     overflow-x: hidden;
-    height: 90%;
+    height: 80%;
     width: 100%;
-    position: absolute;
-    bottom: 40px;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    margin-top: 10vh;
 `;
 
 const Image = styled.img`
-    height: 40vh;
+    height: auto;
+    width: 100%
     display: block;
     margin: 0 auto;
+    position: absolute;
+    bottom: 0;
+    @media (max-width: 768px) {
+      height: auto;
+      min-width: 300%;
+      right: -30vw;  
+  }
 `;
 
 const AdditionalText = styled.div`
