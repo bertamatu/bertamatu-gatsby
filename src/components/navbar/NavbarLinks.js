a5f13dc95d36639256b6a9fb9efa18b7d39ab53d
Berta <info.berta@gmail.com>
2023-03-05 18:44:02 +0200

    hide projects

--- src/components/navbar/NavbarLinks.js
+++ src/components/navbar/NavbarLinks.js
@@ -7,12 +7,12 @@ const NavbarLinks = () => {
         <>
             <NavItem to="/">HOME</NavItem>
             <NavItem to="/about">ABOUT</NavItem>
-            <NavItem to="/projects">PROJECTS</NavItem>
+            {/* <NavItem to="/projects">PROJECTS</NavItem> */}
             {/* <NavItem to="/gallery">GALLERY</NavItem> */}
             {/* <NavItem to="/blog">BLOG</NavItem> */}
             <NavItem to="/links">LINKS</NavItem>
             <NavItem to="/setup">SETUP</NavItem>
-            <NavItem to="/support">SUPPORT</NavItem>
+            {/* <NavItem to="/support">SUPPORT</NavItem> */}
             <NavItem to="/contact">CONTACT</NavItem>
         </>
     );
