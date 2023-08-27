048a442909581bf2f5454feb2a7fe799d9f0fcf5
Berta <info.berta@gmail.com>
2023-08-27 11:16:56 +0300

    update: menu navigation (#59)
    fix window issue in the  Netlify pipeline

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,113 +1,127 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavbarLinks from './NavbarLinks';
 import styled from 'styled-components';
+import { HiMenu } from 'react-icons/hi';
+import { CgClose } from 'react-icons/cg';
+import SocialIcons from '../buttons/SocialIcons';
+
+const isBrowser = typeof window !== "undefined";
 
 const Navbar = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(isBrowser ? window.innerWidth <= 768 : false);
 
-    return (
-        <Navigation>
-            <Toggle
-                navbarOpen={navbarOpen}
-                onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-                {navbarOpen ? <Hamburger open /> : <Hamburger />}
-            </Toggle>
-            {navbarOpen ? (
-                <Navbox>
-                    <NavbarLinks />
-                </Navbox>
-            ) : (
-                <Navbox open>
-                    <NavbarLinks />
-                </Navbox>
-            )}
-        </Navigation>
-    );
+  useEffect(() => {
+    if (isBrowser) {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+        if (!isMobile && navbarOpen) {
+          setNavbarOpen(false);
+        }
+      };
+
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
+  }, [navbarOpen, isMobile]);
+
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <Navigation>
+      <Toggle onClick={toggleNavbar}>
+        {isMobile ? (
+          navbarOpen ? <CloseIcon /> : <HamburgerMobileIcon />
+        ) : null}
+        <NavContainer open={navbarOpen && isMobile}>
+          {isMobile && (
+            <header>
+              <CloseIcon onClick={toggleNavbar} />
+            </header>
+          )}
+          <NavbarLinks />
+          {isMobile && (
+            <Footer>
+              <SocialIcons />
+            </Footer>
+          )}
+        </NavContainer>
+      </Toggle>
+      {!isMobile && <NavbarLinks />}
+    </Navigation>
+  );
 };
 
 export default Navbar;
 
 const Navigation = styled.nav`
-    height: 5vh;
-    display: flex;
-    position: relative;
-    justify-content: center;
-    text-transform: uppercase;
-    padding: 5vh 3vw;
-    z-index: 2;
-    background-color: black;
-    @media (max-width: 768px) {
-        position: sticky;
-        height: 7vh;
-        top: 0;
-        left: 0;
-        right: 0;
-        left: 0;
-    }
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  text-transform: uppercase;
+  background-color: var(--light-gray);
+
+  @media (max-width: 768px) {
+    position: sticky;
+    height: 60px;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
 `;
 
 const Toggle = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100vw;
+  cursor: pointer;
+
+  @media (min-width: 769px) {
     display: none;
-    height: 100%;
-    cursor: pointer;
-    padding: 0 7vw;
-
-    @media (max-width: 768px) {
-        display: flex;
-    }
+  }
 `;
 
-const Navbox = styled.div`
-    display: flex;
-    height: 100%;
-    align-items: center;
+const NavContainer = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  background: var(--light-gray);
+  height: 100%;
+  width: 100%;
+  transition: all 0.3s ease;
+  top: 0;
+  top: ${props => (props.open ? '0' : '-100%')};
 
-    @media (max-width: 768px) {
-        flex-direction: column;
-        position: fixed;
-        width: 100%;
-        justify-content: flex-start;
-        padding-top: 15vh;
-        background: #000;
-        transition: all 0.3s ease-in;
-        top: 0;
-        left: ${props => (props.open ? '-100%' : '0')};
-    }
+  @media (max-width: 768px) {
+    padding-top: 100px;
+  }
 `;
 
-const Hamburger = styled.div`
-    background-color: #fff;
-    width: 30px;
-    height: 4px;
-    transition: all 0.3s linear;
-    align-self: center;
-    position: relative;
-    align-self: center;
-    margin-right: 80vw;
-    transform: ${props => (props.open ? 'rotate(-45deg)' : 'inherit')};
+const SharedIconStyle = `
+  color: var(--black-color);
+  font-size: 26px;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  cursor: pointer;
+`;
 
-    ::before,
-    ::after {
-        width: 30px;
-        height: 4px;
-        background-color: #fff;
-        content: '';
-        position: absolute;
-        transition: all 0.3s linear;
-    }
+const HamburgerMobileIcon = styled(HiMenu)`
+  ${SharedIconStyle}
+`;
 
-    ::before {
-        transform: ${props =>
-            props.open
-                ? 'rotate(-90deg) translate(-10px, 0px)'
-                : 'rotate(0deg)'};
-        top: -10px;
-    }
-    
-    ::after {
-        opacity: ${props => (props.open ? '0' : '1')};
-        transform: ${props => (props.open ? 'rotate(90deg) ' : 'rotate(0deg)')};
-        top: 10px;
-    }
+const CloseIcon = styled(CgClose)`
+  ${SharedIconStyle}
+`;
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
