50e3b66476710de2f82b403183faf59423b85bda
Berta <info.berta@gmail.com>
2023-08-27 10:59:27 +0300

    fix window issue in the  Netlify pipeline

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,107 +6,118 @@ import { CgClose } from 'react-icons/cg';
 import SocialIcons from '../buttons/SocialIcons';
 
 const Navbar = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
-    const isMobile = window.innerWidth <= 768;
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined' &&!isMobile && navbarOpen) {
-            setNavbarOpen(false);
-        }
-    }, [isMobile, navbarOpen]);
-
-    const toggleNavbar = () => {
-        setNavbarOpen(!navbarOpen);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+      if (!isMobile && navbarOpen) {
+        setNavbarOpen(false);
+      }
     };
 
-    return (
-        <Navigation>
-            {isMobile ? (
-                <Toggle onClick={toggleNavbar}>
-                    {!navbarOpen ? <HamburgerMobileIcon /> : <CloseIcon />}
-                    <NavContainer open={navbarOpen}>
-                        <header>
-                            <CloseIcon onClick={toggleNavbar} />
-                        </header>
-                        <NavbarLinks />
-                        <Footer>
-                            <SocialIcons />
-                        </Footer>
-                    </NavContainer>
-                </Toggle>
-            ) : (
-                <NavbarLinks />
-            )}
-        </Navigation>
-    );
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [navbarOpen, isMobile]);
+
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <Navigation>
+      <Toggle onClick={toggleNavbar}>
+        {isMobile ? (
+          navbarOpen ? <CloseIcon /> : <HamburgerMobileIcon />
+        ) : null}
+        <NavContainer open={navbarOpen && isMobile}>
+          {isMobile && (
+            <header>
+              <CloseIcon onClick={toggleNavbar} />
+            </header>
+          )}
+          <NavbarLinks />
+          {isMobile && (
+            <Footer>
+              <SocialIcons />
+            </Footer>
+          )}
+        </NavContainer>
+      </Toggle>
+      {!isMobile && <NavbarLinks />}
+    </Navigation>
+  );
 };
 
 export default Navbar;
 
 const Navigation = styled.nav`
-    z-index: 1;
-    display: flex;
-    justify-content: center;
-    text-transform: uppercase;
-    background-color: var(--light-gray);
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  text-transform: uppercase;
+  background-color: var(--light-gray);
 
-    @media (max-width: 768px) {
-        position: sticky;
-        height: 60px;
-        top: 0;
-        left: 0;
-        right: 0;
-    }
+  @media (max-width: 768px) {
+    position: sticky;
+    height: 60px;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
 `;
 
 const Toggle = styled.div`
-    display: none;
-    height: 100%;
-    width: 100vw;
-    cursor: pointer;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100vw;
+  cursor: pointer;
 
-    @media (max-width: 768px) {
-        display: flex;
-        align-items: center;
-    }
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
 const NavContainer = styled.div`
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    background: var(--light-gray);
-    height: 100%;
-    width: 100%;
-    transition: all 0.3s ease;
-    top: 0;
-    top: ${props => (props.open ? '0' : '-100%')};
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  background: var(--light-gray);
+  height: 100%;
+  width: 100%;
+  transition: all 0.3s ease;
+  top: 0;
+  top: ${props => (props.open ? '0' : '-100%')};
 
-    @media (max-width: 768px) {
-        padding-top: 100px;
-    }
+  @media (max-width: 768px) {
+    padding-top: 100px;
+  }
 `;
 
 const SharedIconStyle = `
-    color: var(--black-color);
-    font-size: 26px;
-    position: absolute;
-    top: 15px;
-    right: 15px;
-    cursor: pointer;
+  color: var(--black-color);
+  font-size: 26px;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  cursor: pointer;
 `;
 
 const HamburgerMobileIcon = styled(HiMenu)`
-    ${SharedIconStyle}
+  ${SharedIconStyle}
 `;
 
 const CloseIcon = styled(CgClose)`
-    ${SharedIconStyle}
+  ${SharedIconStyle}
 `;
 
 const Footer = styled.footer`
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
