e56df2bcafe1d6a6ade6575f022002c51da9193d
Berta <info.berta@gmail.com>
2023-11-02 13:40:37 +0200

    improve best practices (#67)
    Co-authored-by: Berta

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,10 @@ import Seo from '../components/Seo';
 import SocialIcons from '../components/buttons/SocialIcons';
 import HomePageLogo from '../components/common/HomePageLogo';
 import ConnectButton from '../components/buttons/Connect';
-// import { graphql } from 'gatsby';
-// import { GatsbyImage } from 'gatsby-plugin-image';
 
 const IndexPage = ({ data }) => (
     <HomePage>
         <Layout>
-            <Image>
-                {/* <GatsbyImage
-                    image={data.file.childImageSharp.gatsbyImageData}
-                    alt="bertacodes"
-                /> */}
-                <img
-                    src="https://media.giphy.com/media/uB86ZyWQsnFSGYe2sA/giphy.gif"
-                    alt="bertacodes"
-                    style={{filter: "hue-rotate(100deg)", margin: "0"}}
-                />
-            </Image>
             <Seo title="Home" />
             <HomePageLogo />
             <ConnectButton />
@@ -29,16 +16,6 @@ const IndexPage = ({ data }) => (
         </Layout>
     </HomePage>
 );
-
-// export const data = graphql`
-//     query BackgroundImage {
-//         file(relativePath: { eq: "flws.png" }) {
-//             childImageSharp {
-//                 gatsbyImageData(layout: FULL_WIDTH)
-//             }
-//         }
-//     }
-// `;
 
 const HomePage = styled.section`
     overflow-y: hidden;
@@ -49,13 +26,6 @@ const HomePage = styled.section`
     flex-direction: column;
     justify-content: center;
     margin-top: 5vh;
-`;
-
-const Image = styled.div`
-    max-width: 300px;
-    margin: 0 auto;
-    // box-shadow: rgba(0, 0, 0, 0.45) 20px 25px 0 -20px;
-    // box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;
 `;
 
 export default IndexPage;
