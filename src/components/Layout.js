a177d7f0806a1bfd57be861a75cee2d9478f2e81
Berta <info.berta@gmail.com>
2024-10-18 11:52:26 +0300

    improve setup page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,107 +10,109 @@ import GlobalStyles from '../style/GlobalStyles';
 import { Helmet } from 'react-helmet';
 
 const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-        query SiteTitleQuery {
-            site {
-                siteMetadata {
-                    title
-                    menuLinks {
-                        link
-                        name
-                    }
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
+            link
+            name
+          }
         }
-    `);
+      }
+    }
+  `);
 
-    const location = useLocation();
-    const [isMobileView, setIsMobileView] = useState(false);
+  const location = useLocation();
+  const [isMobileView, setIsMobileView] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobileView(window.innerWidth < 768);
-        };
-
-        handleResize();
-        window.addEventListener('resize', handleResize);
-
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const sectionStyle = {
-        marginTop: '10vh',
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
     };
 
-    const cookieConsentStyle = {
-        background: 'rgb(244 244 244)',
-        fontSize: '16px',
-        color: '#000',
-        width: '100%',
-        border: '1px solid white',
-        borderRadius: '4px',
-        justifyContent: 'center',
-        padding: `${isMobileView ? '0 30px' : '0 250px'}`,
-    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
-    const buttonStyle = {
-        background: '#000',
-        color: '#fff',
-        borderRadius: '4px',
-        padding: '16px',
-        fontWeight: '900'
-    };
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-    return (
-        <>
-            <Helmet>
-                <html lang="en" />    
-                <title>BERTA.CODES</title>
-                <meta name="description" content="Berta Matuliauskiene's portfolio." />
-                <meta name="keywords" content="bertacodes, berta.codes, berta matuliauskiene, wecodeui" />
-                <meta charset="UTF-8" />
-                <meta name="author" content="Berta Matuliauskiene" />
-            </Helmet>
-            <GlobalStyles />
-            <section style={sectionStyle}>
-                <Header siteTitle={data.site.siteMetadata.title} />
-                <main>{children}</main>
-                <CookieConsent
-                    name="Accept cookies"
-                    id="rcc-confirm-button"
-                    aria-label="Accept cookies"
-                    location="bottom"
-                    buttonText="Accept and Close"
-                    cookieName="gatsby-gdpr-google-analytics"
-                    style={cookieConsentStyle}
-                    buttonStyle={buttonStyle}
-                    onAccept={() => {
-                        initializeAndTrack(location);
-                    }}
-                >
-                    <div style={{ textAlign: 'justify' }}>
-                        We want to let you know that we use cookies on this
-                        website. To agree, please click the "Accept" button. You
-                        can withdraw your consent at any time by changing your
-                        browser settings.{' '}
-                        <a
-                            href="https://www.iubenda.com/privacy-policy/96553161/cookie-policy"
-                            target="_blank"
-                            rel="noreferrer"
-                            name="Cookie Policy"
-                            aria-label="Cookie Policy"        
-                        >
-                            Cookie Policy.
-                        </a>
-                    </div>
-                </CookieConsent>
-            </section>
-        </>
-    );
+  const sectionStyle = {
+    marginTop: '10vh',
+  };
+
+  const cookieConsentStyle = {
+    background: 'rgb(244 244 244)',
+    fontSize: '16px',
+    color: '#000',
+    width: '100%',
+    border: '1px solid white',
+    borderRadius: '4px',
+    justifyContent: 'center',
+    padding: `${isMobileView ? '0 30px' : '0 250px'}`,
+  };
+
+  const buttonStyle = {
+    background: '#000',
+    color: '#fff',
+    borderRadius: '4px',
+    padding: '16px',
+    fontWeight: '900',
+  };
+
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        <title>BERTA.CODES</title>
+        <meta name="description" content="Berta Matuliauskiene's portfolio." />
+        <meta
+          name="keywords"
+          content="bertacodes, berta.codes, berta matuliauskiene, wecodeui"
+        />
+        <meta charset="UTF-8" />
+        <meta name="author" content="Berta Matuliauskiene" />
+      </Helmet>
+      <GlobalStyles />
+      <section style={sectionStyle}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main>{children}</main>
+        <CookieConsent
+          name="Accept cookies"
+          id="rcc-confirm-button"
+          aria-label="Accept cookies"
+          location="bottom"
+          buttonText="Accept and Close"
+          cookieName="gatsby-gdpr-google-analytics"
+          style={cookieConsentStyle}
+          buttonStyle={buttonStyle}
+          onAccept={() => {
+            initializeAndTrack(location);
+          }}
+        >
+          <div style={{ textAlign: 'justify' }}>
+            We want to let you know that we use cookies on this website. To
+            agree, please click the "Accept" button. You can withdraw your
+            consent at any time by changing your browser settings.{' '}
+            <a
+              href="https://www.iubenda.com/privacy-policy/96553161/cookie-policy"
+              target="_blank"
+              rel="noreferrer"
+              name="Cookie Policy"
+              aria-label="Cookie Policy"
+            >
+              Cookie Policy.
+            </a>
+          </div>
+        </CookieConsent>
+      </section>
+    </>
+  );
 };
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
--- src/pages/roadmaps.js
+++ src/pages/roadmaps.js
@@ -3,39 +3,182 @@ import Layout from '../components/Layout';
 import styled from 'styled-components';
 
 const Roadmap = () => {
-
   return (
     <Layout>
       <Container>
-        <Text>
-         Let's transform your coding dreams into reality together!
-        </Text>
-        <a href='https://www.bertacodes.com/products/front-end-developer-roadmap'>
-          <img 
-            src='https://www.bertacodes.com/cdn/shop/files/Screenshot2024-03-09at15.52.47.png?v=1709992398&width=823' 
-            style={{ maxHeight: '500px' }} 
-            alt='bertacodes front-end developer roadmap'
+        <a href="https://www.wecodeui.com/products/front-end-developer-roadmap">
+          <Image
+            src="https://www.wecodeui.com/cdn/shop/files/Screenshot2024-03-09at15.52.47.png?v=1709992398"
+            alt="bertacodes Berta Matuliauskiene front-end developer roadmap"
           />
         </a>
+        <Text>
+          <br />
+          <a href="https://www.wecodeui.com/products/front-end-developer-roadmap">
+            <h3>FRONT-END DEVELOPER ROADMAP</h3>
+          </a>
+          <header>
+            <h3>Why Choose My Guide?</h3>
+          </header>
+          <section>
+            <p>
+              With over <strong>3.5 years of experience</strong>, I understand
+              the challenges of starting in Front-end development. My guide is
+              crafted to boost your confidence and minimize stress. It's
+              basically a guide and onboarding manual in one.
+            </p>
+            <p>
+              You will not only learn some tech stack but also get familiar with
+              other tools you will encounter in your first developer job! This
+              guide includes{' '}
+              <strong>free access to the WECODEUI Discord community</strong>.
+            </p>
+          </section>
+
+          <section>
+            <h3>Tech Stack Covered</h3>
+            <strong>
+              HTML5 / SCSS / BOOTSTRAP / TAILWIND / JS / GIT / REACTJS / MUI /
+              TYPESCRIPT / JEST / CYPRESS / NETLIFY... and much more.
+            </strong>
+            <br /> <br />
+            <p>
+              So, whether you're a Junior Developer looking to level up or a
+              newcomer to the world of coding, this roadmap has you covered.
+            </p>
+          </section>
+
+          <section>
+            <h3>What You'll Learn</h3>
+            <ul>
+              <li>Essential aspects of forming a coding habit</li>
+              <li>
+                Navigating <strong>Tutorial Hell</strong>
+              </li>
+              <li>Preventing Developer Burnout</li>
+              <li>
+                Effective <strong>Study Plans</strong> and{' '}
+                <strong>Study Schedules</strong>
+              </li>
+              <li>
+                Mastering <strong>Time Management</strong> to optimize your
+                learning journey
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Tools & Techniques</h3>
+            <p>
+              Unlock the power of Google with advanced search techniques and
+              explore the latest AI Tools like ChatGPT and GitHub Copilot to
+              enhance your development workflow. Dive into the world of Code
+              Editors, including IntelliJ IDEA and VS Code, and master the
+              intricacies of Front-End Developer Tools like Chrome DevTools and
+              Lighthouse.
+            </p>
+          </section>
+          <section>
+            <h3>Front-End Roadmap</h3>
+            <ul>
+              <li>
+                Understanding <strong>Web Foundations</strong>
+              </li>
+              <li>
+                Mastering <strong>Fundamentals</strong> like HTML5, CSS3, and
+                JavaScript (including ES6 and Clean Code principles)
+              </li>
+              <li>
+                Version control with <strong>Git and GitHub</strong>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Advanced Topics</h3>
+            <ul>
+              <li>
+                Technologies like{' '}
+                <strong>Node.js, React.js, Redux, and TypeScript</strong>
+              </li>
+              <li>
+                Practical insights into{' '}
+                <strong>React Router, MUI, and Redux DevTools</strong>
+              </li>
+              <li>
+                Essential Testing and Quality Assurance techniques with{' '}
+                <strong>Jest and Cypress</strong>
+              </li>
+              <li>
+                Build Tools like <strong>Vite and Webpack</strong>
+              </li>
+              <li>
+                Understanding <strong>Authentication Strategies</strong> in Web
+                Development
+              </li>
+              <li>
+                Deployment techniques using platforms like{' '}
+                <strong>Netlify and Gatsby.js</strong>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2>Boost Your Skills</h2>
+            <p>
+              Enhance your <strong>Documentation skills</strong> and embrace
+              Agile Development methodologies like <strong>Scrum</strong>. Build
+              your Portfolio and explore Optional Tools like{' '}
+              <strong>Docker, Postman, NGINX, and GraphQL</strong>.
+            </p>
+          </section>
+          <section>
+            <h2>Additional Resources</h2>
+            <p>
+              Access additional resources including Courses, Articles, and Tools
+              like <strong>CS50</strong>, and explore avenues for Freelancing.
+            </p>
+          </section>
+          <footer>
+            <h6>
+              &copy; 2024 Front-End Developer Guide by Berta Matuliauskiene. All
+              rights reserved.
+            </h6>
+          </footer>
+        </Text>
       </Container>
     </Layout>
   );
 };
 
 const Container = styled.section`
-  margin-top: 8vh;
-  text-align: center;
   display: flex;
-  font-size: 12px;
-  flex-direction: column;
-  padding: 4vw;
-    @media (max-width: 768px) {
-      margin-top: 10vh;
+  flex-direction: row;
+  margin: 0 auto;
+  height: 90vh;
+  width: 80%;
+  margin-top: 8vh;
+  margin-bottom: 60px;
+  @media (max-width: 768px) {
+    margin-top: 10vh;
+    justify-content: center;
+    flex-wrap: wrap;
   }
 `;
 
-const Text = styled.div`
-margin-bottom: 2vh
+const Image = styled.img`
+  min-width: 200px;
+  max-width: 300px;
+  box-shadow:
+    rgba(0, 0, 0, 0.19) 0px 10px 20px,
+    rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  border-radius: 4px;
+  margin-right: 40px;
+`;
+
+const Text = styled.article`
+  font-size: 15px;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    text-align: justify;
+  }
 `;
 
 export default Roadmap;
